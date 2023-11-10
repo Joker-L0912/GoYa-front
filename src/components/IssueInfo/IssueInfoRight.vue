4a38ab42cad2a0ee88c2cad3cc664fe8cd01c0f5
LimOum0u <1522282814@qq.com>
2023-11-10 23:34:50 +0800

    问题详情页面右侧顶部

--- src/components/IssueInfo/IssueInfoRight.vue
+++ src/components/IssueInfo/IssueInfoRight.vue
@@ -21,15 +21,37 @@ const changeLike = () => {
               :color='isAttention ? "#0C66E4" : ""' />
     </v-sheet>
     <v-sheet class='ma-2 pa-2 icon-container'
-             :onclick='changeLike'>
+             @click='changeLike'>
       <v-icon icon='mdi-thumb-up-outline'
               :color='islike ? "#0C66E4" : ""' />
     </v-sheet>
     <v-sheet class='ma-2 pa-2 icon-container'>
       <v-icon icon='mdi-share-variant-outline' />
     </v-sheet>
-    <v-sheet class='ma-2 pa-2 icon-container'>
-      <v-icon icon='mdi-dots-horizontal' />
+    <v-sheet class='ma-2 pa-2 icon-container'
+             :onclick='changeLike'>
+      <v-menu location='bottom'>
+        <template #activator='{ props }'>
+          <div v-bind='props'
+               class='text-body-1 px-2'>
+            <div class='d-flex justify-center align-center'>
+              <div>
+                <v-icon icon='mdi-dots-horizontal '
+                        size='20'
+                        h-100 />
+              </div>
+            </div>
+          </div>
+        </template>
+        <v-list :nav='true'>
+          <v-list-item>
+            <v-list-item-title>aaa</v-list-item-title>
+            <v-list-item-title>aaa</v-list-item-title>
+            <v-list-item-title>aaa</v-list-item-title>
+            <v-list-item-title>aaa</v-list-item-title>
+          </v-list-item>
+        </v-list>
+      </v-menu>
     </v-sheet>
   </div>
 </template>
