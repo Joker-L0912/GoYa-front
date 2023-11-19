28a432ea03ffa100d9081056023b60426f177376
LimOum0u <1522282814@qq.com>
2023-11-19 22:46:38 +0800

    头部菜单，下一步要修改菜单类型

--- src/components/Header/index.vue
+++ src/components/Header/index.vue
@@ -6,7 +6,7 @@ const itemList = ref([ // 水平一级菜单栏的菜单
   {
     name: 'Home',
     path: '/Home',
-    title: '首页',
+    title: '工作',
   },
   {
     name: 'issue',
@@ -19,6 +19,7 @@ const itemList = ref([ // 水平一级菜单栏的菜单
     title: '测试',
   },
 ])
+const menuList = ref<string[]>(['工作', '项目', '筛选器', '仪表盘', '团队', '计划', '应用'])
 </script>
 
 <template>
@@ -27,35 +28,41 @@ const itemList = ref([ // 水平一级菜单栏的菜单
              :elevation='0'>
     <v-toolbar color='#59666C'
                :flat='true'>
-      <v-app-bar-nav-icon></v-app-bar-nav-icon>
+      <v-app-bar-nav-icon />
 
       <v-sheet class='d-flex align-center bg-transparent'
                height='100'>
-        <v-menu location='top'>
-          <template #activator='{ props }'>
-            <v-btn v-bind='props'
-                   class='text-body-1 px-2'>
-              <div class='d-flex justify-center align-center'>
-                <div class='h-100'>
-                  项目
+        <div class='menu-container'
+             v-for='(item, idx) in menuList'
+             :key='idx'>
+          <v-menu location='top'>
+            <template #activator='{ props }'>
+              <v-btn v-bind='props'
+                     class='text-body-1 px-2'>
+                <div class='d-flex justify-center align-center'>
+                  <div class='h-100'>
+                    {{ item }}
+                  </div>
+                  <div>
+                    <v-icon icon='mdi-chevron-down'
+                            size='20'
+                            h-100 />
+                  </div>
                 </div>
-                <div>
-                  <v-icon icon='mdi-chevron-down'
-                          size='20'
-                          h-100 />
-                </div>
-              </div>
-            </v-btn>
-          </template>
-          <v-list :nav='true'>
-            <v-list-item v-for='(item, index) in itemList'
-                         :key='index'
-                         :value='index'
-                         :to='item.path'>
-              <v-list-item-title>{{ item.title }}</v-list-item-title>
-            </v-list-item>
-          </v-list>
-        </v-menu>
+              </v-btn>
+            </template>
+            <v-list :nav='true'>
+              <v-list-item v-for='(item1, index) in itemList'
+                           :key='index'
+                           :value='index'
+                           :to='item1.path'>
+                <v-list-item-title>{{ item1.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+
+        <v-btn class='bg-white'>创建</v-btn>
       </v-sheet>
     </v-toolbar>
   </v-app-bar>
@@ -63,11 +70,7 @@ const itemList = ref([ // 水平一级菜单栏的菜单
 
 <style scoped
        lang='less'>
-.top-nav-bar {
-
-  .top-nav-bar-list {
-    display: flex;
-    align-content: center;
-  }
+.menu-container{
+  margin-left: 10px;
 }
 </style>
